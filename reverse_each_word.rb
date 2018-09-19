f1712238242a7bbd284a101dec51586f76c9a9a9
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-19 09:06:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(sentence)
-  reversed_sentence = []
-  sentence.split(" ").each do |word|
-    reversed_sentence << word.reverse
-   end
-  reversed_sentence.join(" ")
+  sentence.split(" ").collect {|word| word.reverse}.join(" ")
 end
